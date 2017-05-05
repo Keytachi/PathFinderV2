835a2ddd08dc338fd16e6fa31bb1fe1e18de1ac7
Brandon <Brandonle1992@gmail.com>
2017-05-05 02:36:46 -0500

    Inventory fixes. Creating a composition of Inventory and having an arraylist of InventoryItem allow us to display the content within the Bag.
    Will need to work on fixing up the stack items since it is doubling instead of just adding.

--- src/com/rpgfoundation/Secondary/Inventory.java
+++ src/com/rpgfoundation/Secondary/Inventory.java
@@ -1,7 +1,6 @@
 package com.rpgfoundation.Secondary;
 
 
-import com.rpgfoundation.Gear.Weapon;
 import com.rpgfoundation.Secondary.Modify.InventoryItem;
 
 import java.util.ArrayList;
@@ -11,28 +10,55 @@ import java.util.ArrayList;
  */
 public class Inventory{
 
-    private ArrayList<InventoryItem> bag;
+    private ArrayList<InventoryItem> bagContent = new ArrayList<>();
+
+
+    private int max_BagSpace;
+    private int current_BagUsage;
 
     public Inventory()
     {
         this.max_BagSpace = 20;
         this.current_BagUsage = 0;
-        this.bag = new ArrayList<>();
     }
-
-    private int max_BagSpace;
-    private int current_BagUsage;
 
     public void addItem(InventoryItem o)
     {
         if (!(getCurrent_BagUsage() >= getMax_BagSpace()))
             {
-                System.out.println("Inventory added: " + o);
-                setCurrent_BagUsage(getCurrent_BagUsage() + 1);
-                bag.add(o);
+                System.out.println("Inventory added: " + o + " x" + o.getCount());
+                if(o.stack() == InventoryItem.Stack.YES && ifExist(o) )
+                {
+                    InventoryItem a = null;
+                    for(int j = 0; j < getBagContent().size(); j++)
+                    {
+                        if(o.getName() == getBagContent().get(j).getName())
+                        {
+                            a = getBagContent().get(j);
+                        }
+                    }
+                    a.setCount(a.getCount()+ o.getCount());
+                }
+                else {
+                    setCurrent_BagUsage(getCurrent_BagUsage() + 1);
+                    bagContent.add(o);
+                }
             }
         else
                 System.out.println("Bag is full");
+    }
+
+    public void removeItem(InventoryItem o)
+    {
+        if(ifExist(o))
+        {
+            bagContent.remove(o);
+            System.out.println("You have removed " + o + " from inventory");
+        }
+        else{
+            System.out.println(o + " is not in inventory");
+        }
+
     }
 
     public int getCurrent_BagUsage() {
@@ -44,8 +70,16 @@ public class Inventory{
     public int getMax_BagSpace() {
         return max_BagSpace;
     }
-    public ArrayList<InventoryItem> getBag()
+    public ArrayList<InventoryItem> getBagContent()
     {
-        return bag;
+        return bagContent;
+    }
+
+    public boolean ifExist(InventoryItem o)
+    {
+        for(int i = 0; i < getBagContent().size(); i++)
+            if(o.getName() == getBagContent().get(i).getName())
+                return true;
+        return false;
     }
 }
